1125c337c29fde8095448e5fc6477565b7f1c258
DESKTOP-84K80GL\LG <sojd0316@naver.com>
2024-02-13 17:33:53 +0900

    Player Inventory, Skill, State min, max 반영하여 수정

--- Assets/Script/Joo/PlayerSkill.cs
+++ Assets/Script/Joo/PlayerSkill.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class PlayerSkill : MonoBehaviour
 {
     // 신체 능력
-    float Fitness = 0f;  // 체력
-    float Strength = 0f;  // 근력
+    protected float Min_Fitness = 0f;  // 최소 체력
+    protected float Max_Fitness = 10f;  // 최대 체력
+    protected float Min_Strength = 0f;  // 최소 근력
+    protected float Max_Strength = 10f;  // 최대 근력
 
     // 생존 능력
     // float Fishing = 0f;  // 낚시
@@ -14,33 +16,50 @@ public class PlayerSkill : MonoBehaviour
     // float Foraging = 0f;  // 채집
 
     // 운동 능력
-    float Sprinting = 0f;  // 달리기
-    float Lightfooted = 0f;  // 민첩함
-    float Nimble = 0f;  // 날렵함
-    float Sneaking = 0f;  // 은밀함
+    protected float Min_Sprinting = 0f;  // 달리기
+    protected float Max_Sprinting = 10f;  
+    protected float Min_Lightfooted = 0f;  // 민첩함
+    protected float Max_Lightfooted = 10f;  
+    protected float Min_Nimble = 0f;  // 날렵함
+    protected float Max_Nimble = 10f; 
+    protected float Min_Sneaking = 0f;  // 은밀함
+    protected float Max_Sneaking = 10f;  
 
     // 전투 능력
-    float Axe = 0f;  // 도끼
-    float LongBlunt = 0f;  // 긴 둔기
-    float ShortBlunt = 0f;  // 짧은 둔기
-    float LongBlade = 0f;  // 장검
-    float ShortBlade = 0f;  // 단검
-    float Spear = 0f;  // 창
-    float Maintenance = 0f;  // 물건관리
+    protected float Min_Axe = 0f;  // 도끼
+    protected float Max_Axe = 10f;
+    protected float Min_LongBlunt = 0f;  // 긴 둔기
+    protected float Max_LongBlunt = 10f;
+    protected float Min_ShortBlunt = 0f;  // 짧은 둔기
+    protected float Max_ShortBlunt = 10f;
+    protected float Min_LongBlade = 0f;  // 장검
+    protected float Max_LongBlade = 10f;
+    protected float Min_ShortBlade = 0f;  // 단검
+    protected float Max_ShortBlade = 10f;
+    protected float Min_Spear = 0f;  // 창
+    protected float Max_Spear = 10f;
+    protected float Min_Maintenance = 0f;  // 물건관리
+    protected float Max_Maintenance = 10f;
 
     // 제작 능력
-    float Carpentry = 0f;  // 목공
-    float Cooking = 0f;  // 요리
-    float Farming = 0f;  // 농사
-    float FirstAid = 0f;  // 의료
-    float Electrical = 0f;  // 전기공학
+    protected float Min_Carpentry = 0f;  // 목공
+    protected float Max_Carpentry = 10f;
+    protected float Min_Cooking = 0f;  // 요리
+    protected float Max_Cooking = 10f;
+    protected float Min_Farming = 0f;  // 농사
+    protected float Max_Farming = 10f;
+    protected float Min_FirstAid = 0f;  // 의료
+    protected float Max_FirstAid = 10f;
+    protected float Min_Electrical = 0f;  // 전기공학
+    protected float Max_Electrical = 10f;
     // float Metalworking = 0f;  // 금속용접
     // float Mechanics = 0f;  // 차량정비
     // float Tailoring = 0f;  // 재단술
 
     // 총기
-    float Aiming = 0f;  // 조준
-    float Reloading = 0f;  // 재장전
-
+    protected float Min_Aiming = 0f;  // 조준
+    protected float Max_Aiming = 10f;
+    protected float Min_Reloading = 0f;  // 재장전
+    protected float Max_Reloading = 10f;
 
 }
--- Assets/Script/Joo/PlayerState.cs
+++ Assets/Script/Joo/PlayerState.cs
@@ -4,21 +4,55 @@ using UnityEngine;
 
 public class PlayerState : MonoBehaviour
 {
-    float Left_hand = 100.0f; // ¿Þ¼Õ
-    float Right_hand = 100.0f; // ¿À¸¥¼Õ
-    float Left_forearm = 100.0f; // ¿ÞÆÈ¸ñ
-    float Right_forearm = 100.0f;// ¿À¸¥ÆÈ¸ñ
-    float Left_upper_arm = 100.0f; // ¿Þ ÆÈ¶Ò
-    float Right_upper_arm = 100.0f; // ¿À¸¥ ÆÈ¶Ò
-    float upper_torso = 100.0f; // °¡½¿
-    float Lower_torso = 100.0f; // º¹ºÎ
-    float Head = 100.0f; // ¸Ó¸®
-    float Neck = 100.0f; // ¸ñ
-    float Groin = 100.0f; // »çÅ¸±¸´Ï
-    float Left_thigh = 100.0f; // ¿Þ Çã¹÷Áö
-    float Right_thigh = 100.0f; // ¿À¸¥ Çã¹÷Áö
-    float Left_shin = 100.0f; // ¿Þ Á¤°­ÀÌ
-    float Right_shin = 100.0f; // ¿À¸¥ Á¤°­ÀÌ
-    float Left_foot = 100.0f; // ¿Þ¹ß
-    float Right_foot = 100.0f; // ¿À¸¥¹ß
+    // ¿Þ¼Õ
+    float Min_Left_hand = 0.0f;
+    float Max_Left_hand = 100.0f;
+    // ¿À¸¥¼Õ
+    float Min_Right_hand = 0.0f;
+    float Max_Right_hand = 100.0f;
+    // ¿ÞÆÈ¸ñ
+    float Min_Left_forearm = 0.0f; 
+    float Max_Left_forearm = 100.0f;
+    // ¿À¸¥ÆÈ¸ñ
+    float Min_Right_forearm = 0.0f;
+    float Max_Right_forearm = 100.0f;
+    // ¿Þ ÆÈ¶Ò
+    float Min_Left_upper_arm = 0.0f; 
+    float Max_Left_upper_arm = 100.0f;
+    // ¿À¸¥ ÆÈ¶Ò
+    float Min_Right_upper_arm = 0.0f; 
+    float Max_Right_upper_arm = 100.0f;
+    // °¡½¿
+    float Min_upper_torso = 0.0f; 
+    float Max_upper_torso = 100.0f;
+    // º¹ºÎ
+    float Min_Lower_torso = 0.0f;
+    float Max_Lower_torso = 100.0f;
+    // ¸Ó¸®
+    float Min_Head = 0.0f; 
+    float Max_Head = 100.0f;
+    // ¸ñ
+    float Min_Neck = 0.0f; 
+    float Max_Neck = 100.0f;
+    // »çÅ¸±¸´Ï
+    float Min_Groin = 0.0f;
+    float Max_Groin = 100.0f;
+    // ¿Þ Çã¹÷Áö
+    float Min_Left_thigh = 0.0f;
+    float Max_Left_thigh = 100.0f;
+    // ¿À¸¥ Çã¹÷Áö
+    float Min_Right_thigh = 0.0f;
+    float Max_Right_thigh = 100.0f;
+    // ¿Þ Á¤°­ÀÌ
+    float Min_Left_shin = 0.0f;
+    float Max_Left_shin = 100.0f;
+    // ¿À¸¥ Á¤°­ÀÌ
+    float Min_Right_shin = 0.0f;
+    float Max_Right_shin = 100.0f;
+    // ¿Þ¹ß
+    float Min_Left_foot = 0.0f;
+    float Max_Left_foot = 100.0f;
+    // ¿À¸¥¹ß
+    float Min_Right_foot = 0.0f;
+    float Max_Right_foot = 100.0f; 
 }
