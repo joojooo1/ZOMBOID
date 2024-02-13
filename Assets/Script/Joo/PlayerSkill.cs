c03676c2d21a51b18e12b9ef72f92454e48de53d
DESKTOP-84K80GL\LG <sojd0316@naver.com>
2024-02-13 16:52:06 +0900

    Player 관련 Inventory, Movement, Skill, State 변수 생성

---
+++ Assets/Script/Joo/PlayerSkill.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSkill : MonoBehaviour
+{
+    // 신체 능력
+    float Fitness = 0f;  // 체력
+    float Strength = 0f;  // 근력
+
+    // 생존 능력
+    // float Fishing = 0f;  // 낚시
+    // float Trapping = 0f;  // 함정
+    // float Foraging = 0f;  // 채집
+
+    // 운동 능력
+    float Sprinting = 0f;  // 달리기
+    float Lightfooted = 0f;  // 민첩함
+    float Nimble = 0f;  // 날렵함
+    float Sneaking = 0f;  // 은밀함
+
+    // 전투 능력
+    float Axe = 0f;  // 도끼
+    float LongBlunt = 0f;  // 긴 둔기
+    float ShortBlunt = 0f;  // 짧은 둔기
+    float LongBlade = 0f;  // 장검
+    float ShortBlade = 0f;  // 단검
+    float Spear = 0f;  // 창
+    float Maintenance = 0f;  // 물건관리
+
+    // 제작 능력
+    float Carpentry = 0f;  // 목공
+    float Cooking = 0f;  // 요리
+    float Farming = 0f;  // 농사
+    float FirstAid = 0f;  // 의료
+    float Electrical = 0f;  // 전기공학
+    // float Metalworking = 0f;  // 금속용접
+    // float Mechanics = 0f;  // 차량정비
+    // float Tailoring = 0f;  // 재단술
+
+    // 총기
+    float Aiming = 0f;  // 조준
+    float Reloading = 0f;  // 재장전
+
+
+}
