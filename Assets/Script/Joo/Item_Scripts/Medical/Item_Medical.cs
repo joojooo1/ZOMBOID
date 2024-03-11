a9c42ac1c9f84c071dc547ef1c68d308928c8cce
DESKTOP-84K80GL\LG <sojd0316@naver.com>
2024-03-11 20:42:52 +0900

    데이터베이스 수정

---
+++ Assets/Script/Joo/Item_Scripts/Medical/Item_Medical.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Item_Medical : ScriptableObject
+{
+    
+
+
+
+
+
+}
---
+++ Assets/Script/Joo/Item_Scripts/Medical/Medical_Ins.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Medical_Ins : MonoBehaviour
+{
+    [SerializeField]
+    private List<Item_Medical> medical_Ins;
+    [SerializeField]
+    private GameObject Medical_Prefab;
+
+
+
+}
