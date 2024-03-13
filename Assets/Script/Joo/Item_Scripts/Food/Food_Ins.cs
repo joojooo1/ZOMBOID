f0c53b8c3de3ff3b9dc9fdf015ef27d3e84f6f0e
DESKTOP-84K80GL\LG <sojd0316@naver.com>
2024-03-13 16:07:51 +0900

    medical database커밋

--- Assets/Script/Joo/Item_Scripts/Food/Food_Ins.cs
+++ Assets/Script/Joo/Item_Scripts/Food/Food_Ins.cs
@@ -19,13 +19,22 @@ public class Food_Ins : MonoBehaviour
     // bool로 기존에 생성된 적 있는 오브젝트인지 체크
     public void Set_Generating_item(Location_Type location_Type)
     {        
-        System.Random rand = new System.Random();
-        int Count = rand.Next(0, 6);
+        List<Item_Food> food_List = new List<Item_Food>();
+        for(int i = 0; i < food_Ins.Count; i++)
+        {
+            if (food_Ins[i].Location == location_Type)
+                food_List.Add(food_Ins[i]);
+        }
+
+        System.Random rand_Count = new System.Random();
+        int Count = rand_Count.Next(0, 8);  // 0~8개 생성
 
         for (int i = 0; i < Count; i++)
         {
             System.Random rand_item = new System.Random();
-            int item_number = rand.Next(0, 6);
+            int item = rand_item.Next(0, food_List.Count);
+
+            Instan(food_List[item]);
         }
 
     }
