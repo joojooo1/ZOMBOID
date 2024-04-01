29e48aa856b9248d5d4de54945094d589c00f73e
DESKTOP-84K80GL\LG <sojd0316@naver.com>
2024-04-01 21:04:12 +0900

    Merge remote-tracking branch 'origin/Joo' into Joo
    # Conflicts:
    #	Assets/Scenes/SampleScene.unity

---
+++ Assets/Script/Joo/UI/UI_Player_State.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Player_State : MonoBehaviour
+{
+    public Sprite[] player_state_SpriteArray;
+
+    private void Start()
+    {
+
+    }
+    private void Update()
+    {
+        
+    }
+}
+
+/*
+ [0] scratch
+ [1] bandage
+ [2] bandagedirty
+ [3] bite
+ [4] burn
+ [5] fracture
+ [6] stitches
+ ------------------
+ [7] infection
+ [8] deepwound
+ [9] glass
+ [10] splint
+ 
+ */
