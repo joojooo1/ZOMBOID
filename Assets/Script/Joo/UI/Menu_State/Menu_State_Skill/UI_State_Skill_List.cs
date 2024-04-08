98e4861b4a758e8ae225266bf0346ff231b44299
Eunjoo <sojd0316@naver.com>
2024-04-08 16:38:54 +0900

    State Skill 수정

--- Assets/Script/Joo/UI/Menu_State/Menu_State_Skill/UI_State_Skill_List.cs
+++ Assets/Script/Joo/UI/Menu_State/Menu_State_Skill/UI_State_Skill_List.cs
@@ -6,4 +6,9 @@ public class UI_State_Skill_List : MonoBehaviour
 {
     public UnityEngine.UI.Slider Slider;
     public UnityEngine.UI.Image Image;
+
+    public void Start()
+    {
+        //Slider.value = 0;
+    }
 }
