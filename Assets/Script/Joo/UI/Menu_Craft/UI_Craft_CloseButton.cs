0e304bd09392c57747daef0258cfeb0b63243649
Eunjoo <sojd0316@naver.com>
2024-04-24 18:16:16 +0900

    Joo script 임시커밋

---
+++ Assets/Script/Joo/UI/Menu_Craft/UI_Craft_CloseButton.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UI_Craft_CloseButton : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        UI_Craft.UI_Craft_main.Craft_window.SetActive(false);
+        UI_Craft.UI_Craft_main.Craft_Button_current.sprite = UI_Craft.UI_Craft_main.Craft_Button_Image[0];
+    }
+}
