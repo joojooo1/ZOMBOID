06bf113003e3ba4a8d72082316deaa9c389c51ec
Eunjoo <sojd0316@naver.com>
2024-04-25 17:16:27 +0900

    240425 스크립트 커밋

--- Assets/Script/Joo/UI/Menu_Craft/UI_Craft_Ins_Prefab.cs
+++ Assets/Script/Joo/UI/Menu_Craft/UI_Craft_Ins_Prefab.cs
@@ -1,19 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
 
-public class UI_Craft_Ins_Prefab : MonoBehaviour
+public class UI_Craft_Ins_Prefab : MonoBehaviour, IDragHandler, IEndDragHandler
 {
     public UnityEngine.UI.Image item_Image;
+    public GameObject background;
     public Crafting_item item_Info;
+
+    RectTransform recrTr;
 
     public void Set_Info(Crafting_item item)
     {
+        recrTr = GetComponent<RectTransform>();
         item_Image.sprite = item.item_Image;
         item_Info = item;
     }
 
+    public void OnDrag(PointerEventData eventData)
+    {
+        background.SetActive(false);
+        recrTr.anchoredPosition += (eventData.delta * 0.4f);
+    }
 
-
-
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        background.SetActive(true);
+        recrTr.localPosition = Vector3.zero;
+    }
 }
--- Assets/Script/Joo/UI/Menu_Craft/UI_Craft_window.cs
+++ Assets/Script/Joo/UI/Menu_Craft/UI_Craft_window.cs
@@ -29,13 +29,14 @@ public class UI_Craft_window : MonoBehaviour
     private void OnEnable()
     {
         ui_Craft_Window = this;
-        scrollbar.value = 1;
 
         Origin_list = new List<Crafting_item>();
         Crafting_item_Prefab_list = new List<UI_Craft_Prefab>();
         Crafting_Ingredients_Prefab_list = new List<UI_Craft_Prefab>();
 
         list_ins();
+
+        scrollbar.value = 1;
     }
 
     public void list_ins()
@@ -111,6 +112,7 @@ public class UI_Craft_window : MonoBehaviour
             Destroy(child.gameObject);
         }
 
+        Selected_item = null;
         Origin_list.Clear();
         Crafting_item_Prefab_list.Clear();
         Clear_Ingredients_Window();
@@ -123,6 +125,7 @@ public class UI_Craft_window : MonoBehaviour
             if (Crafting_item_Prefab_list[i].item_index == index)
             {
                 Crafting_item_Prefab_list[i].Choice = true;
+                Selected_item = Crafting_item_Prefab_list[i];
             }
             else
             {
@@ -184,11 +187,36 @@ public class UI_Craft_window : MonoBehaviour
         Crafting_Ingredients_Prefab_list.Clear();
     }
 
-    public void Ins_item_Window(UI_Craft_Prefab _item)
+    UI_Craft_Prefab Selected_item = null;
+
+    public void Ins_item_Window()
     {
-        GameObject obj = null;
-        obj = Instantiate(Crafting_Ins_item, Crafting_Ins_Window);
-        UI_Craft_Ins_Prefab item = obj.GetComponent<UI_Craft_Ins_Prefab>();
-        item.Set_Info(_item.item_Info);
+        bool check = false;
+        if(Crafting_Ins_Window != null)
+        {
+            foreach (Transform child in Crafting_Ins_Window)
+            {
+                if(child.gameObject.activeSelf) check = true;
+                else check = false;
+            }
+        }
+
+        if (Selected_item != null && check == false)
+        {
+            if (type != Crafting_type.Crafting_Furniture)
+            {
+                GameObject obj = null;
+                obj = Instantiate(Crafting_Ins_item, Crafting_Ins_Window);
+                UI_Craft_Ins_Prefab item = obj.GetComponent<UI_Craft_Ins_Prefab>();
+                item.Set_Info(Selected_item.item_Info);
+            }
+            else
+            {
+                // 가구는 제작하기 버튼 누르면 맵에서 바로 설치하는 기능 구현해야함
+                // 제작창에서 Crafting_Ins_Window 없음
+            }
+
+        }
+
     }
 }
