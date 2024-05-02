2683be8bad9c2e8db5b42ba7b306cd245087c026
Eunjoo <sojd0316@naver.com>
2024-05-03 02:43:17 +0900

    240502 DB 수정 및 추가

--- Assets/preset/Joo/Inventory_Database/Item_DataBase.cs
+++ Assets/preset/Joo/Inventory_Database/Item_DataBase.cs
@@ -35,6 +35,8 @@ public class Item_DataBase : MonoBehaviour
     public List<Item_Clothing> clothing_Ins;
     public List<Item_Gardening> gardening_Ins;
     public List<Item_Container> Container_Ins;
+    public List<Item_ETC> ETC_Ins;
+    public List<Item_Furniture> furniture_Ins;
 
     public void Requesting_Baisics(Item_Type item_Type, short ID)
     {
