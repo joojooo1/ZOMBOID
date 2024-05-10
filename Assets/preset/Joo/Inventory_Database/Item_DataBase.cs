c91c79aa02f11e7cb4604ae40a1a331ca928539f
Jeyun <rksrksekek4@gmail.com>
2024-05-10 21:18:08 +0900

    32123

--- Assets/preset/Joo/Inventory_Database/Item_DataBase.cs
+++ Assets/preset/Joo/Inventory_Database/Item_DataBase.cs
@@ -6,17 +6,34 @@ public class Item_DataBase : MonoBehaviour
 {
     public static Item_DataBase item_database;
 
+    //public enum Item_Type
+    //{
+    //    empty = 0,
+    //    Food_Ins = 1,
+    //    Medical_Ins = 2,
+    //    Weapons_Ins = 3,
+    //    Literature_Ins = 4,
+    //    Electronics_Ins = 5,
+    //    Clothing_Ins = 6,
+    //    Gardening_Ins = 7,
+    //    Container_Ins = 8,
+    //    ETC_Ins = 9
+    //}
+
     public enum Item_Type
     {
-        empty = 0,
-        Food_Ins = 1,
-        Medical_Ins = 2,
-        Weapons_Ins = 3,
-        Literature_Ins = 4,
-        Electronics_Ins = 5,
-        Clothing_Ins = 6,
-        Gardening_Ins = 7,
-        Container_Ins = 8
+        Empty = 0,
+        Farming = 1,
+        food = 2,
+        Normal = 3,
+        literature = 4,
+        Tool = 5,
+        clothing = 6,
+        Container = 7,
+        Electronics = 8,
+        Medical = 9,
+        Furniture = 10,
+        weapon = 11
     }
 
     private void Awake()
@@ -63,16 +80,31 @@ public class Item_DataBase : MonoBehaviour
     {
         switch (item_Type)
         {
-            case 1:
-                Sprite Img1 = food_Ins[ID].Food_Image[0];
-                //Debug.Log("IDB image send");
-                return Img1;
+            case 2:
+                Sprite Img2 = food_Ins[ID].Food_Image[0];
+                return Img2;
+            case 3:
+                Sprite Img3 = ETC_Ins[ID].ETC_Image;
+                return Img3;
+            case 4:
+                Sprite Img4 = literature_Ins[ID].Literature_Image;
+                return Img4;
+            case 6:
+                Sprite Img6 = clothing_Ins[ID].ClothingImage;
+                return Img6;
+            case 7:
+                Sprite Img7 = Container_Ins[ID].Container_Image;
+                return Img7;
             case 8:
-                Sprite Img8 = Container_Ins[ID].Container_Image;
-                //Debug.Log("IDB image send");
+                Sprite Img8 = electronics_Ins[ID].Electronics_Image;
                 return Img8;
+            case 9:
+                Sprite Img9 = medical_Ins[ID].Medical_Image;
+                return Img9;
+            case 10:
+                Sprite Img10 = furniture_Ins[ID].Furniture_Image;
+                return Img10;
             default:
-                //Debug.Log("IDB image null");
                 return null;
         }
     }
@@ -80,27 +112,33 @@ public class Item_DataBase : MonoBehaviour
     {
         switch (item_Type)
         {
-            case 1:
-                short num1 = 0;
-                num1 += (short)(food_Ins[ID].Width*100);
-                num1 += (short)0;
-                num1 += (short)(food_Ins[ID].Height);
-                return num1;
-            case 8: //∝ 啊规率 抗寇贸府
-                short num8 = 0;
+            case 2:
+                short num2 = 0;
+                num2 += (short)(food_Ins[ID].Width * 100);
+                num2 += (short)0;
+                num2 += (short)(food_Ins[ID].Height);
+                return num2;
+            case 7: //∝ 啊规率 抗寇贸府
+                short num7 = 0;
                 if (ID < 8)
                 {
-                    num8 += (short)(((int)((Container_Ins[ID].Width) / 2)) * 100);
-                    num8 += (short)0;
-                    num8 += (short)((int)((Container_Ins[ID].Height) / 2));
+                    num7 += (short)(((int)((Container_Ins[ID].Width) / 2)) * 100);
+                    num7 += (short)0;
+                    num7 += (short)((int)((Container_Ins[ID].Height) / 2));
                 }
                 else
                 {
-                    num8 += (short)((Container_Ins[ID].Width) * 100);
-                    num8 += (short)0;
-                    num8 += (short)(Container_Ins[ID].Height);
+                    num7 += (short)((Container_Ins[ID].Width) * 100);
+                    num7 += (short)0;
+                    num7 += (short)(Container_Ins[ID].Height);
                 }
-                return num8;
+                return num7;
+            case 9:
+                short num9 = 0;
+                num9 += (short)(medical_Ins[ID].Width * 100);
+                num9 += (short)0;
+                num9 += (short)(medical_Ins[ID].Height);
+                return num9;
             default:
                 return 0;
         }
@@ -111,11 +149,17 @@ public class Item_DataBase : MonoBehaviour
 
         switch (Type)
         {
-            case 1:
+            case 2:
                 Width = (int)food_Ins[ID].Width;
                 break;
-            case 8:
+            case 9:
+                Width = (int)medical_Ins[ID].Width;
+                break;
+            case 7:
                 Width = (int)Container_Ins[ID].Width;
+                break;
+            case 3:
+                Width = (int)ETC_Ins[ID].Width;
                 break;
         }
         return Width;
@@ -126,11 +170,17 @@ public class Item_DataBase : MonoBehaviour
 
         switch (Type)
         {
-            case 1:
+            case 2:
                 Height = (int)food_Ins[ID].Height;
                 break;
-            case 8:
+            case 9:
+                Height = (int)medical_Ins[ID].Height;
+                break;
+            case 7:
                 Height = (int)Container_Ins[ID].Height;
+                break;
+            case 3:
+                Height = (int)ETC_Ins[ID].Height;
                 break;
         }
         return Height;
