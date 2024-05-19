bd0cb3fecf9a39057e21de1147266f22b79efbac
Eunjoo <sojd0316@naver.com>
2024-05-19 21:38:18 +0900

    240519

--- Assets/Script/Joo/UI/UI_Option/UI_GameScreen.cs
+++ Assets/Script/Joo/UI/UI_Option/UI_GameScreen.cs
@@ -7,9 +7,18 @@ public class UI_GameScreen : MonoBehaviour
     public UnityEngine.UI.Text Screen_change_button_text;
     public bool Fullscreen_Mode = true;
     bool Current_mode = false;
+
+    public GameObject Ui_title;
+    public GameObject Button_title;
     public void OnEnable()
     {
         Current_mode = Fullscreen_Mode;
+
+        if (Ui_title.activeSelf)
+            Button_title.SetActive(false);
+        else
+            Button_title.SetActive(true);
+
     }
 
     public void Fullscreen()
